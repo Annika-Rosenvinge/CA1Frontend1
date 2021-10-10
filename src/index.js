02cb024cd3cc0446dbb8099ef4500f0e0efba1d4
Annika Rosenvinge Jespersen <Annika2000@live.dk>
2021-10-10 15:49:24 +0200

    create, delete, edit update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import "./style.css"
 import "bootstrap/dist/css/bootstrap.css"
-import "./jokeFacade"
-import jokeFacade from "./jokeFacade"
-import personFacade from ".personFacade"
+import * as bootstrap from 'bootstrap';
+import '@popperjs/core';
+import $ from 'jquery';
+import personFacade from "./personFacade";
 
 document.getElementById("all-content").style.display = "block"
 
@@ -18,54 +19,151 @@ document.getElementById("all-content").style.display = "block"
 
 
 /* JS Person */
-getAllPersons()
+//henter alle personer
+const dmTable = document.querySelector('#allPersonsRows')
+personFacade.getAll(dmTable)
+//henter personen frem
 document.getElementById("getPersonByIdButton").addEventListener("click", event => getPerson())
+//laver en person
+document.getElementById("addAPersonButton").addEventListener("click", event => createPerson())
+//sletter en person
+document.getElementById("deletePersonByIdButton").addEventListener("click", event =>deletePersonById())
+//redigere en person
+document.getElementById("editPersonButton").addEventListener("click", event => editPerson())
+//billeder
+//document.getElementById("ex1_html").addEventListener("mousemove", event =>personFacade.picture())
 
-/* Finder alle personer*/
-function getAllPersons(){
-  personFacade.getAll().then(persons =>showPersons(persons))
-}
 
-function showPersons(persons){
-  const personsToHTML = persons.map(persons =>
-    `<tr>
-    <td>{person.id}</td>
-    <td>{person.firstname}</td>
-    <td>{person.lastname}</td>
-    <td>{person.email}</td>
-  </tr>`
-    )
-    document.getElementById("allPersonsRows").innerHTML = personsToHTML.join("")
-}
+
 
 /* Finder en bestemt person */
 function getPerson(){
-  const userID = document.getElementById("getPersonByIdtext").value
-  personFacade.getPerson(userID).then(person =>{
+  const personID = document.getElementById("getPersonByIdText").value
+  personFacade.getPerson(personID).then(person =>{
     document.getElementById("getPersonByIdDiv").innerHTML =
     `<h5 style = "margin-top:20px">Her er personen</h5>
     <table>
       <tr><td>Id:</td><td>${person.id}</td></tr>
-      <tr><td>Fornavn:</td><td>${person.firstname}</td></tr>
-      <tr><td>Efternavn:</td><td>${person.lastname}</td></tr>
+      <tr><td>Fornavn:</td><td>${person.firstName}</td></tr>
+      <tr><td>Efternavn:</td><td>${person.lastName}</td></tr>
       <tr><td>Email:</td><td>${person.email}</td></tr>
+      <tr><td>Telefon nummer:</td><td>${person.phones[0].number}</td></tr>
+      <tr><td>Telefon beskrivelse:</td><td>${person.phones[0].description}</td></tr>
+      <tr><td>Adresse</td>:</td><td>${person.address.street}</td></tr>
+      <tr><td>Mere info:</td><td>${person.address.addInfo}</td></tr>
+      <tr><td>By:</td><td>${person.address.cityInfo.city}</td></tr>
+      <tr><td>Postnummer:</td><td>${person.address.cityInfo.zipCode}</td></tr>
+      <tr><td>Hobby navn:</td><td>${person.hobbies[0].name}</td></tr>
+      <tr><td>Hobby link:</td><td>${person.hobbies[0].wikiLink}</td></tr>
+      <tr><td>Hobby kategori:</td><td>${person.hobbies[0].category}</td></tr>
+      <tr><td>Hobby type:</td><td>${person.hobbies[0].type}</td></tr>
     </table>`
+    console.log(person)
   })
-  .catch(error => errorHandling)
+  .catch(error => errorHandling(error))
 }
+
+/* Create person */
+function createPerson() {
+  const firstName = document.getElementById("personFirstName").value;
+  const lastName = document.getElementById("personLastName").value;
+  const email = document.getElementById("personEmail").value;
+  const hobbyName = document.getElementById("personHobbyName").value;
+  const hobbyWikiLink = document.getElementById("personHobbyWikilink").value;
+  const hobbyCategory = document.getElementById("personHobbyCategory").value;
+  const hobbyType = document.getElementById("personHobbyType").value;
+  const phones = document.getElementById("personPhone").value;
+  const phonesdes = document.getElementById("personPhoneDes").value;
+  const street = document.getElementById("personStreet").value;
+  const addInfo = document.getElementById("personAddInfo").value;
+  const city = document.getElementById("personCity").value;
+  const zipCode = document.getElementById("personzip").value;
+
+  const hobby = {
+    "name": hobbyName,
+    "wikiLink": hobbyWikiLink,
+    "category": hobbyCategory,
+    "type": hobbyType,
+  }
+  const hobbies =[
+      hobby
+  ]
+  const phone = {
+    "number": phones,
+    "description": phonesdes
+  }
+  const phoneList = [
+    phone
+  ]
+  const cityInfo = {
+    "zipCode": zipCode,
+    "city": city,
+  }
+  const address = {
+    "street": street,
+    "addInfo": addInfo,
+    "cityInfo": cityInfo,
+  }
+  const person = {
+    "firstName": firstName,
+    "lastName": lastName,
+    "email": email,
+    "hobby": hobbies,
+    "phone": phoneList,
+    "address": address
+
+  }
+  console.log(JSON.stringify(person));
+  personFacade.createPerson(person)
+}
+
+/* Edit person*/
+function editPerson(){
+  const personID = document.getElementById("editPersonId").value
+  const firstName = document.getElementById("editPersonFirstName").value;
+  const lastName = document.getElementById("editPersonLastName").value;
+  const person = {
+    "id": personID,
+    "firstName": firstName,
+    "lastName":lastName
+  }
+  console.log(person)
+  personFacade.editPersonNames(id,person)
+}
+
+/* Delete person */
+function deletePersonById(){
+  const personID = document.getElementById("deletePersonByIdText").value
+  personFacade.deletePerson(personID).then(person =>{
+    document.getElementById("getPersonByIdDiv").innerHTML = `
+    <h5 style = "margin-top:20px">>Den person der er blevet slettet</h5>
+      <table>
+        <tr><td>Id:</td><td>${person.id}</td></tr>
+        <tr><td>Fornavn:</td><td>${person.firstName}</td></tr>
+        <tr><td>Efternavn:</td><td>${person.lastName}</td></tr>
+        <tr><td>Email:</td><td>${person.email}</td></tr>
+      </table>
+    `
+    console.log(person)
+  })
+  .catch(error => errorHandling(error))
+}
+
 
 function errorHandling(err){
   if (err.status){
     err.fullError.then(e => {
       $("#errorMessage").text(e.msg);
-      $("#errorModal").modal();
     })
   }
   else{
     $("#errorMessage").text("Network error. The user API is not responding.");
-    $("#errorModal").modal();
   }
 }
+function pictureInsert(){
+
+}
+
 
 /* 
 Do NOT focus on the code below, UNLESS you want to use this code for something different than
@@ -74,18 +172,18 @@ the Period2-week2-day3 Exercises
 
 function hideAllShowOne(idToShow) {
   document.getElementById("about_html").style = "display:none"
-  document.getElementById("classdiagram").style = "display:none"
+  document.getElementById("ex1_html").style = "display:none"
   document.getElementById("ex2_html").style = "display:none"
-  document.getElementById("persons").style = "display:none"
+  document.getElementById("ex3_html").style = "display:none"
   document.getElementById(idToShow).style = "display:block"
 }
 
 function menuItemClicked(evt) {
   const id = evt.target.id;
   switch (id) {
-    case "classdiagram": hideAllShowOne("classdiagram"); break
+    case "ex1": hideAllShowOne("ex1_html"); break
     case "ex2": hideAllShowOne("ex2_html"); break
-    case "persons": hideAllShowOne("persons"); break
+    case "ex3": hideAllShowOne("ex3_html"); break
     default: hideAllShowOne("about_html"); break
   }
   evt.preventDefault();
--- src/personFacade.js
+++ src/personFacade.js
@@ -1,26 +1,81 @@
+
 const URL = 'https://www.test.juliusmadsen.dk/devops-starter/api/person';
 
-
+function URLfunction(){
+    fetch(URL)
+        .then(handleHttpErrors)
+        .then(data => console.log(data.name))
+        .catch(error =>{
+            if(error.status){
+                error.fullError.then(e=> console.log(e.msg))
+            }
+            else{
+                console.log("Network Error")
+            }
+        });
+}
+function getStatus() {
+    return fetch(URL + "/status")
+        .then(res => handleHttpErrors(res))
+}
 
 function getPerson(id){
-    return fetch(URL + '/' + id).then(result => handleHttpErrors(result))
+    return fetch(URL + '/' + id).
+    then(result => handleHttpErrors(result))
 }
 
-function getAll(){
-    return fetch(URL + '/all').then(result => handleHttpErrors(result) )
+function getAll(DOMElement){
+    return fetch('https://www.test.juliusmadsen.dk/devops-starter/api/person/all')
+        .then(function (response){
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data.all)
+            const all = data.all
+            const personsListTable = document.querySelector('#personlist')
+            //const tableTbody = document.querySelector('#allPersonsRows')
+            for (let persons of all) {
+                let tr = DOMElement.insertRow(0);
+                let cell1 =tr.insertCell(0)
+                    let cell2 =tr.insertCell(1)
+                    let cell3 =tr.insertCell(2)
+                    let cell4 =tr.insertCell(3)
+                    let cell5 =tr.insertCell(4)
+                    let cell6 =tr.insertCell(5)
+                    let cell7 = tr.insertCell(6)
+                    let cell8 =tr.insertCell(7)
+                    let cell9 =tr.insertCell(8)
+                    let cell10 =tr.insertCell(9)
+                    let cell11 = tr.insertCell(10)
+                    cell1.innerHTML = persons.id;
+                    cell2.innerHTML = persons.firstName;
+                    cell3.innerHTML = persons.lastName;
+                    cell4.innerHTML = persons.email;
+                    cell5.innerHTML = persons.hobbies[0].name;
+                    cell6.innerHTML = persons.phones[0].number;
+                    cell7.innerHTML = persons.phones.description;
+                    cell8.innerHTML = persons.address.street;
+                    cell9.innerHTML = persons.address.addInfo;
+                    cell10.innerHTML = persons.address.cityInfo.city;
+                    cell11.innerHTML = persons.address.cityInfo.zipCode;
+            }
+        })
 }
 
-function createPerson(){
-
+function createPerson(person){
+    const options = makeOptions('POST', person)
+    return fetch(URL+'/create', options).then(result => handleHttpErrors(result))
 }
 
-function editPersonNames(){
-
+function editPersonNames(id, person){
+    const options = makeOptions('PUT', person)
+    return fetch(URL + '/update/' + id, options)
+        .then (result => handleHttpErrors(result))
 }
 
-const personFacade = {
-    getPerson,
-    getAll,
+function deletePerson(id){
+    const options = makeOptions('DELETE', {id})
+    return fetch(URL + '/delete/' + id, options).then(result => handleHttpErrors(result))
 }
 
 function makeOptions(method, body) {
@@ -44,4 +99,20 @@ function handleHttpErrors(res) {
     return res.json();
 }
 
+function picture(){
+    let img = document.createElement('img')
+    img.src = "CA1 Class Diagram (1).png";
+    let src = document.getElementById("picture");
+    src.appendChild(img)
+}
+
+const personFacade = {
+    getStatus,
+    getPerson,
+    getAll,
+    createPerson,
+    editPersonNames,
+    deletePerson,
+    picture
+}
 export default personFacade;
